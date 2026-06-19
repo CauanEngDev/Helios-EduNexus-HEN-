a132d977b8c6c37f5417bcf075c84af17e7de76f
Cauan EngDev <reisu.d.cauan@proton.me>
2026-06-18 22:08:27 -0300

    Backend Setup Criação do backend em Springboot

---
+++ heliosedunexus-backend/src/main/java/com/cauandev/heliosedunexus_backend/HeliosedunexusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cauandev.heliosedunexus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeliosedunexusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeliosedunexusBackendApplication.class, args);
+	}
+
+}
---
+++ heliosedunexus-backend/src/test/java/com/cauandev/heliosedunexus_backend/HeliosedunexusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cauandev.heliosedunexus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliosedunexusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
